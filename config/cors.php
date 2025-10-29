17f1fc96217fbf77670680e600bcf366ca38d556
JuanJoIC2025 <i1125818@continental.edu.pe>
2025-10-29 16:13:08 -0500

    cambio de CORS

--- config/cors.php
+++ config/cors.php
@@ -6,16 +6,21 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200', 'http://127.0.0.1:4200'],
+    'allowed_origins' => [
+        'http://localhost:4200',
+        'http://127.0.0.1:4200',
+        'https://estilodorado.onrender.com',
+        'https://backendestilodorado.onrender.com',
+    ],
 
     'allowed_origins_patterns' => [],
-    
-    'allowed_headers' => ['Content-Type','X-Requested-With','Authorization','Accept','Origin'],
+
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
     'supports_credentials' => true,
-    
+
 ];
